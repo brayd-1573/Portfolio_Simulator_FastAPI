afe93587e4532880a61b5b82e17b6dc19a9c4cf7
brayd-1573 <98786062+brayd-1573@users.noreply.github.com>
2024-07-10 10:36:30 -0500

    commit

--- api/main.py
+++ api/main.py
@@ -1,9 +1,7 @@
 from fastapi import FastAPI
-from numba import jit
 
 app = FastAPI()
 
-@jit
 @app.get("/run")
 async def run_model(strategy: str):
     # Placeholder for your model invocation logic
